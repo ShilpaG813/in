9fbb513182dc20690c02c5aaaf256e96ae2ba506
Shilpa.G <shilpagshilpag813@gmail.com>
2023-07-29 19:23:30 +0530

    first commit

---
+++ df.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data={
+    'Subjects':["Java","Python"],
+    'Fees':[1000,2000]
+}
+df=pd.DataFrame(data)
+print(df)
+
+df_fees=df['Fees'].map(lambda x:x*2)
+print("After multiplying \n", df_fees)
+print("After filtering")
+
